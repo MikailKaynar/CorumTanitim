a122289b8f306461b667144a9a313f0f66a48c80
Mikail kaynar <75529228+MikailKaynar@users.noreply.github.com>
2023-01-05 15:51:08 +0300

    CorumTanitim

---
+++ CorumTanitim/CorumTanitim/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CorumTanitim.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Gezilecekyerler()
+        {
+            return View();
+        }
+        public ActionResult BelediyeBaskanlarımız()
+        {
+            return View();
+        }
+        public ActionResult Tarihimiz()
+        {
+            return View();
+        }
+        public ActionResult Yemekler()
+        {
+            return View();
+        }
+    }
+}
